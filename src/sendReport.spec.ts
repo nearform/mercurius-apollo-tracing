c5b17ef2887c1062e20b204f7fb950db2b0cbb56
Jiří Špác <capajj@gmail.com>
2021-08-25 19:14:16 +0200

    convert the spec to tap

--- src/sendReport.spec.ts
+++ src/sendReport.spec.ts
@@ -1,55 +1,53 @@
-import { sendReport } from './sendReport'
 import http from 'http'
+
 import { Report } from 'apollo-reporting-protobuf'
+import tap from 'tap'
 
-describe('sendReport', () => {
-  let server
+import { sendReport } from './sendReport'
 
-  it('should send', async () => {
-    const fakeReport = {
-      header: {
-        hostname: 'www.example.com',
-        graphRef: 'alskncka384u1923e8uino1289jncvo019n'
-      },
-      tracesPerQuery: {
-        '# Foo\nquery Foo { user { email pets { name } } }': {
-          trace: []
-        }
+tap.test('sendReport sends', async (t) => {
+  const fakeReport = {
+    header: {
+      hostname: 'www.example.com',
+      graphRef: 'alskncka384u1923e8uino1289jncvo019n'
+    },
+    tracesPerQuery: {
+      '# Foo\nquery Foo { user { email pets { name } } }': {
+        trace: []
       }
     }
+  }
 
-    const requestListener = function (req, res) {
-      const chunks: any[] = []
-      req.on('data', (chunk: any) => chunks.push(chunk))
-      req.on('end', () => {
-        const data = Buffer.concat(chunks)
+  const requestListener = function (req, res) {
+    const chunks: any[] = []
+    req.on('data', (chunk: any) => chunks.push(chunk))
+    req.on('end', () => {
+      const data = Buffer.concat(chunks)
 
-        const reportDecoded = Report.decode(data)
+      const reportDecoded = Report.decode(data)
 
-        expect(reportDecoded).toMatchSnapshot(fakeReport)
-      })
-      res.writeHead(200)
-      res.end()
-      // TODO decode report and assert
-    }
+      t.matchSnapshot(reportDecoded)
+    })
+    res.writeHead(200)
+    res.end()
+    // TODO decode report and assert
+  }
 
-    server = http.createServer(requestListener)
-    server.listen(3334)
+  const server = http.createServer(requestListener)
+  server.listen(3334)
 
-    const res = await sendReport(
-      fakeReport,
-      {
-        endpointUrl: 'http://localhost:3334',
-        apiKey: 'fakeKey',
-        graphRef: 'myGraph@current'
-      },
-      {} as any
-    )
+  const res = await sendReport(
+    fakeReport,
+    {
+      endpointUrl: 'http://localhost:3334',
+      apiKey: 'fakeKey',
+      graphRef: 'myGraph@current'
+    },
+    {} as any
+  )
 
-    expect(res.statusCode).toBe(200)
-  })
+  t.equal(res.statusCode, 200)
 
-  afterAll(() => {
-    server.close()
-  })
+  server.close()
+  t.end()
 })
