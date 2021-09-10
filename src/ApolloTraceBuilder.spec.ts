34ceb349c92b80625bb2500ebb0482d6210021b3
Andrew Wood <andrew.wood@nearform.com>
2021-09-10 15:32:50 +0100

    test timers

--- src/ApolloTraceBuilder.spec.ts
+++ src/ApolloTraceBuilder.spec.ts
@@ -1,30 +1,59 @@
 import { DocumentNode, SelectionNode } from 'graphql'
 import tap from 'tap'
+import sinon from 'sinon'
 
 import { ApolloTraceBuilder } from './ApolloTraceBuilder'
 
-tap.test('builds the query signature', async (t) => {
-  const testSelectionNode: SelectionNode = {
-    kind: 'Field',
-    name: { kind: 'Name', value: 'test', loc: { start: 1, end: 5 } },
-    arguments: [],
-    directives: [],
-    loc: { start: 1, end: 5 }
-  } as any
+const testSelectionNode: SelectionNode = {
+  kind: 'Field',
+  name: { kind: 'Name', value: 'test', loc: { start: 1, end: 5 } },
+  arguments: [],
+  directives: [],
+  loc: { start: 1, end: 5 }
+} as any
 
-  const document: DocumentNode = {
-    kind: 'Document',
-    definitions: [
-      {
-        kind: 'OperationDefinition',
-        operation: 'query',
-        selectionSet: {
-          kind: 'SelectionSet',
-          selections: [testSelectionNode]
-        }
+const document: DocumentNode = {
+  kind: 'Document',
+  definitions: [
+    {
+      kind: 'OperationDefinition',
+      operation: 'query',
+      selectionSet: {
+        kind: 'SelectionSet',
+        selections: [testSelectionNode]
       }
-    ]
-  }
+    }
+  ]
+}
+
+tap.test('builds the query signature', async (t) => {
   const traceBuilder = new ApolloTraceBuilder(document, {})
   t.equal(traceBuilder.querySignature, '# -\n{test}')
+})
+
+tap.test('timers', async (t) => {
+  let clock
+  t.afterEach(() => {
+    clock.restore()
+  })
+  t.test('start and end times match time change', async (tt) => {
+    const startTime = 1483228800000
+    const tickTime = 10000
+    clock = sinon.useFakeTimers({ now: startTime })
+    const traceBuilder = new ApolloTraceBuilder(document, {})
+    traceBuilder.startTiming()
+
+    clock.tick(tickTime)
+
+    traceBuilder.stopTiming()
+
+    tt.same(traceBuilder.trace.startTime, {
+      seconds: startTime / 1000,
+      nanos: 0
+    })
+    tt.same(traceBuilder.trace.endTime, {
+      seconds: (startTime + tickTime) / 1000,
+      nanos: 0
+    })
+  })
 })
