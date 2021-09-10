b8a31a1e0b7f6ae73120348a79fd8afba1c3aa17
Andrew Wood <andrew.wood@nearform.com>
2021-09-10 14:29:21 +0100

    tests for sendReport

--- src/sendReport.spec.ts
+++ src/sendReport.spec.ts
@@ -1,36 +1,46 @@
 import http from 'http'
+import zlib from 'zlib'
 
 import { Report } from 'apollo-reporting-protobuf'
 import tap from 'tap'
+import {
+  MockAgent,
+  setGlobalDispatcher,
+  getGlobalDispatcher,
+  Dispatcher
+} from 'undici'
+import sinon from 'sinon'
 
 import { sendReport } from './sendReport'
 
-tap.test('sendReport sends', async (t) => {
-  const fakeReport = {
-    header: {
-      hostname: 'www.example.com',
-      graphRef: 'alskncka384u1923e8uino1289jncvo019n'
-    },
-    tracesPerQuery: {
-      '# Foo\nquery Foo { user { email pets { name } } }': {
-        trace: []
-      }
+const fakeReport = {
+  header: {
+    hostname: 'www.example.com',
+    graphRef: 'alskncka384u1923e8uino1289jncvo019n'
+  },
+  tracesPerQuery: {
+    '# Foo\nquery Foo { user { email pets { name } } }': {
+      trace: [],
+      statsWithContext: [],
+      internalTracesContributingToStats: []
     }
   }
+}
 
+tap.test('sendReport encodes the report', async (t) => {
   const requestListener = function (req, res) {
     const chunks: any[] = []
     req.on('data', (chunk: any) => chunks.push(chunk))
     req.on('end', () => {
       const data = Buffer.concat(chunks)
 
-      const reportDecoded = Report.decode(data)
+      const unzippedData = zlib.unzipSync(data)
 
-      t.matchSnapshot(reportDecoded)
+      const reportDecoded = Report.decode(unzippedData)
+      t.same(reportDecoded, fakeReport)
     })
     res.writeHead(200)
     res.end()
-    // TODO decode report and assert
   }
 
   const server = http.createServer(requestListener)
@@ -50,4 +60,70 @@ tap.test('sendReport sends', async (t) => {
 
   server.close()
   t.end()
+})
+
+tap.test('with mocked http', async (t) => {
+  const endpointUrl = 'http://www.example.com'
+
+  let agent: MockAgent
+  let originalDispatcher: Dispatcher
+  t.beforeEach(() => {
+    agent = new MockAgent()
+    agent.disableNetConnect()
+    originalDispatcher = getGlobalDispatcher()
+    setGlobalDispatcher(agent)
+  })
+  t.afterEach(() => {
+    agent.enableNetConnect()
+    setGlobalDispatcher(originalDispatcher)
+  })
+  t.test('thing', async (tt) => {
+    return tt.equal(2, 2)
+  })
+  t.test('sendReport success', async (tt) => {
+    agent
+      .get(endpointUrl)
+      .intercept({
+        path: '/api/ingress/traces',
+        method: 'POST'
+      })
+      .reply(200, {})
+
+    const res = await sendReport(
+      fakeReport,
+      {
+        endpointUrl,
+        apiKey: 'fakeKey',
+        graphRef: 'myGraph@current'
+      },
+      {} as any
+    )
+    return tt.equal(res.statusCode, 200)
+  })
+  t.test('sendReport error', async (tt) => {
+    agent
+      .get(endpointUrl)
+      .intercept({
+        path: '/api/ingress/traces',
+        method: 'POST'
+      })
+      .reply(400, 'something went wrong')
+
+    const logErrorSpy = sinon.spy()
+    const res = await sendReport(
+      fakeReport,
+      {
+        endpointUrl,
+        apiKey: 'fakeKey',
+        graphRef: 'myGraph@current'
+      },
+      {
+        log: {
+          error: logErrorSpy
+        }
+      } as any
+    )
+    tt.equal(res.statusCode, 400)
+    return tt.equal(logErrorSpy.called, true)
+  })
 })
