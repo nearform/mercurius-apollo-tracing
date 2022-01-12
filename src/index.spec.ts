5b333bd47cc039809cb0e38e804ef881e458c75f
gilach <grzegorz@ilach.pl>
2022-01-12 17:54:44 +0100

    chore: add test for referenced fields tracing

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,6 +1,9 @@
 import tap from 'tap'
 import Fastify from 'fastify'
 import fp from 'fastify-plugin'
+import mercurius from 'mercurius'
+
+import { basicResolvers, basicSchema } from '../examples/basicSchema'
 
 import plugin from './index'
 
@@ -48,4 +51,64 @@ tap.test('plugin registration', async (t) => {
       'an Apollo Studio API key is required'
     )
   })
+})
+
+tap.test('trace store', async (t) => {
+  let app
+  t.beforeEach(async () => {
+    app = Fastify()
+    app.register(mercurius, {
+      schema: basicSchema,
+      resolvers: basicResolvers,
+      graphiql: true
+    })
+    app.register(plugin, {
+      apiKey: 'APOLLO_KEY',
+      endpointUrl: 'http://localhost:3334',
+      graphRef: 'APOLLO_GRAPH_ID' + '@' + 'APOLLO_GRAPH_VARIANT',
+      sendReportsImmediately: true
+    })
+
+    // dummy query to get trace store created
+    await app.inject({
+      method: 'POST',
+      url: '/graphql',
+      payload: {
+        query: 'query dummyQuery { word }'
+      }
+    })
+  })
+
+  t.test(
+    'should contain trace with reference to field used in query',
+    async (tt) => {
+      const query = `
+      query testQuery {
+        post {
+          body
+        }
+      }`
+
+      // prevent from emptying the store too soon
+      app.apolloTracingStore.flushTracing = async () => null
+
+      await app.inject({
+        method: 'POST',
+        url: '/graphql',
+        payload: { query }
+      })
+
+      tt.equal(app.apolloTracingStore.traceBuilders.length, 1)
+      tt.match(app.apolloTracingStore.traceBuilders[0], {
+        referencedFieldsByType: {
+          Query: {
+            fieldNames: ['post']
+          },
+          Post: {
+            fieldNames: ['body']
+          }
+        }
+      })
+    }
+  )
 })
